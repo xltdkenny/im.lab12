23710b9558eaa424a039abb494fb07d059c19a8b
kotovskij531@gmail.com <kotovskij531@gmail.com>
2024-06-04 18:23:19 +0700

    Добавьте файлы проекта.

---
+++ ChainsOfMarkov/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ChainsOfMarkov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StopStartButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Номер = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Значение = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StopStartButton
+            // 
+            this.StopStartButton.Location = new System.Drawing.Point(545, 511);
+            this.StopStartButton.Name = "StopStartButton";
+            this.StopStartButton.Size = new System.Drawing.Size(245, 23);
+            this.StopStartButton.TabIndex = 0;
+            this.StopStartButton.Text = "Стоп/Старт";
+            this.StopStartButton.UseVisualStyleBackColor = true;
+            this.StopStartButton.Click += new System.EventHandler(this.StopStartButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Номер,
+            this.Значение});
+            this.dataGridView1.Location = new System.Drawing.Point(545, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(245, 471);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Номер
+            // 
+            this.Номер.HeaderText = "Номер";
+            this.Номер.Name = "Номер";
+            // 
+            // Значение
+            // 
+            this.Значение.HeaderText = "Значение";
+            this.Значение.Name = "Значение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(806, 546);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.StopStartButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "weather";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StopStartButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Номер;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Значение;
+    }
+}
+
---
+++ ChainsOfMarkov/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace ChainsOfMarkov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double[,] Q = { { -0.4, 0.3, 0.1 }, { 0.4, -0.8, 0.4 }, { 0.1, 0.4, -0.5 } };
+        private Random random = new Random();
+        private int currentState = 1;
+        private double[] duration = new double[3];
+        private double totalTime = 0;
+        private int transitionCount = 0;
+        private bool started = false;
+        //Показываем состояние погоды
+        private void showPicture()
+        {
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string imgPath = Path.Combine(basePath, "img");
+
+            switch (currentState)
+            {
+                case 1:
+                    pictureBox1.Image = Image.FromFile(Path.Combine(imgPath, "sunday.png"));
+                    break;
+                case 2:
+                    pictureBox1.Image = Image.FromFile(Path.Combine(imgPath, "cloud.png"));
+                    break;
+                case 3:
+                    pictureBox1.Image = Image.FromFile(Path.Combine(imgPath, "rainy.png"));
+                    break;
+                default:
+                    pictureBox1.Image = Image.FromFile(Path.Combine(imgPath, "rainbow.png"));
+                    break;
+            }
+        }
+        //добавляем данные в таблицу
+        private void UpdateDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                double frequency = duration[i] / totalTime;
+                dataGridView1.Rows.Add(i + 1, frequency);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double rate = -Q[currentState - 1, currentState - 1];
+            double tau = -Math.Log(random.NextDouble()) / rate;
+
+            totalTime += tau;
+            duration[currentState - 1] += tau;
+
+            // Генерируем следующее состояние
+            double randomNumber = random.NextDouble();
+            double cumulativeProbability = 0;
+            int nextState = currentState;
+
+            for (int j = 0; j < Q.GetLength(1); j++)
+            {
+                if (j != currentState - 1)
+                {
+                    cumulativeProbability += Q[currentState - 1, j] / rate;
+                    if (randomNumber <= cumulativeProbability)
+                    {
+                        nextState = j + 1;
+                        break;
+                    }
+                }
+            }
+
+            currentState = nextState;
+            showPicture();
+            UpdateDataGridView();
+
+            transitionCount++;
+            Thread.Sleep(2000);
+        }
+
+        private void StopStartButton_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                if (!started)
+                {
+                    started = true;
+                }
+                timer1.Start();
+            }
+        }
+    }
+}
